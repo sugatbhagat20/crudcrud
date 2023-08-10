040ad8600393274a467badb52d771d0576fecc7b
sugatbhagat20 <sugat.bhagat20@gmail.com>
2023-08-11 02:49:32 +0530

    Get request done

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var form = document.getElementById("form");
 var list = document.getElementById("list");
 
 var nameItem = document.querySelector("#name");
-var amountItem = document.querySelector("#email");
+var emailItem = document.querySelector("#email");
 var phoneItem = document.querySelector("#phone");
 
 form.addEventListener("submit", addItem);
@@ -13,25 +13,11 @@ form.addEventListener("submit", addItem);
 let key = {};
 function addItem(e) {
   e.preventDefault();
-
   key = {
     name: nameItem.value,
-    email: amountItem.value,
-    phone_no: phoneItem.value,
+    email: emailItem.value,
+    phone: phoneItem.value,
   };
-
-  axios
-    .post(
-      "https://crudcrud.com/api/f4728bd998544a728ea461c8bcb5f352/appointmentData",
-      key
-    )
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
   var input1 = document.getElementById("name").value;
   var input2 = document.getElementById("email").value;
   var input3 = document.getElementById("phone").value;
@@ -63,4 +49,52 @@ function addItem(e) {
 
   //append li to ul
   list.appendChild(li);
+  axios
+    .post(
+      "https://crudcrud.com/api/e89dc6211406462e8a6f9858622a3a90/appointmentData",
+      key
+    )
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  renderList();
+});
+
+async function renderList() {
+  try {
+    const users = await axios.get(
+      "https://crudcrud.com/api/e89dc6211406462e8a6f9858622a3a90/appointmentData"
+    );
+
+    users.data.forEach((elem) => {
+      var li = document.createElement("li");
+      li.textContent = `${elem.name} ${elem.email} ${elem.name}`;
+      var deleteBtn = document.createElement("button");
+      var editBtn = document.createElement("button");
+
+      //give classname to the li element
+      li.className = "items";
+      deleteBtn.className = "delete btn btn-dark";
+      editBtn.className = "edit btn btn-info";
+
+      deleteBtn.appendChild(document.createTextNode("Delete"));
+      editBtn.appendChild(document.createTextNode("Edit"));
+
+      //apend the txt
+
+      li.appendChild(deleteBtn);
+      li.appendChild(editBtn);
+
+      //append li to ul
+      list.appendChild(li);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
